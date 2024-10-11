bab91d8a275880a5b1253b42bb2b874071c7ca22
Aaron Bailey <97994050+Aaron-Bailey1202@users.noreply.github.com>
2024-10-11 23:22:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+const refreshBtn = document.getElementById("btnRefresh");
+
+function handleClick() {
+  window.location.reload();
+}
+
+refreshBtn.addEventListener("click", handleClick);
+
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
